b7820d3bd644cb56befb999afed911b6f29b0df5
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2024-08-19 19:46:20 +0600

    added full chatting app design

--- src/components/Chatting/index.jsx
+++ src/components/Chatting/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { EmojiIcon } from "../../svg/Smile";
+import { GalleryIcon } from "../../svg/Gallery";
 
 const Chatting = () => {
   return (
@@ -9,6 +11,25 @@ const Chatting = () => {
             <div className=" w-10 h-10 rounded-full bg-orange-200 overflow-hidden"></div>
             <div>
               <span className=" font-fontRegular text-white">Mamunur</span>
+            </div>
+          </div>
+        </div>
+        <div className=" h-[430px] bg-[#fbfbfbfb] px-6">say something</div>
+        <div className=" bg-[#f5f5f5] py-4">
+          <div className=" bg-white w-[532px] mx-auto rounded-md py-3 flex items-center justify-center gap-x-5">
+            <div className=" flex items-center gap-x-2 w-[15%]">
+              <EmojiIcon />
+              <GalleryIcon />
+            </div>
+            <input
+              type="text"
+              placeholder="type something"
+              className=" w-[60%] p-1 outline-none"
+            />
+            <div className=" w-[15%]">
+              <button className=" bg-[#4A81D3] px-4 py-2 rounded-md font-fontRegular text-sm text-white">
+                Send
+              </button>
             </div>
           </div>
         </div>
