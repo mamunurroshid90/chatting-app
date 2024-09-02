c17f9996e3a0bd34ee41e5b6ba00a500f8238781
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2024-09-02 18:29:13 +0600

    added friend request accept and reject function

--- src/components/FriendRequest/index.jsx
+++ src/components/FriendRequest/index.jsx
@@ -1,6 +1,14 @@
-import { getDatabase, onValue, ref } from "firebase/database";
+import {
+  getDatabase,
+  onValue,
+  push,
+  ref,
+  remove,
+  set,
+} from "firebase/database";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import avatarImg from "../../assets/avatar.jpg";
 
 const FriendRequest = () => {
   const [friendRequestList, setFriendRequestList] = useState([]);
@@ -22,6 +30,20 @@ const FriendRequest = () => {
   }, [db, user.uid]);
 
   // console.log(friendRequestList);
+
+  // Accept request handle
+  const handleAccept = (data) => {
+    set(push(ref(db, "friends")), {
+      ...data,
+    }).then(() => {
+      remove(ref(db, "friendRequest/" + data.id));
+    });
+  };
+
+  // reject friend request handle
+  const handleReject = (data) => {
+    remove(ref(db, "friendRequest/" + data.id));
+  };
   return (
     <>
       <div className=" shadow-md bg-white rounded-md p-5 h-[600px] overflow-y-auto">
@@ -33,16 +55,28 @@ const FriendRequest = () => {
             key={item.id}
           >
             <div className=" flex items-center gap-x-2 mt-3">
-              <div className=" w-12 h-12 rounded-full bg-purple-400 overflow-hidden"></div>
+              <div className=" w-12 h-12 rounded-full bg-purple-400 overflow-hidden">
+                <img
+                  src={item.currentProfile || avatarImg}
+                  alt=""
+                  className=" w-full h-full object-cover"
+                />
+              </div>
               <div className=" font-fontRegular text-black text-md">
                 <h3>{item.senderName} </h3>
               </div>
             </div>
             <div className=" flex items-center gap-x-2">
-              <button className=" bg-[#4A81D3] text-white px-4 py-2 rounded-md font-fontRegular">
+              <button
+                className=" bg-[#4A81D3] text-white px-4 py-2 rounded-md font-fontRegular"
+                onClick={() => handleAccept(item)}
+              >
                 Accept
               </button>
-              <button className=" bg-[#D34A4A] text-white px-4 py-2 rounded-md font-fontRegular">
+              <button
+                className=" bg-[#D34A4A] text-white px-4 py-2 rounded-md font-fontRegular"
+                onClick={() => handleReject(item)}
+              >
                 Reject
               </button>
             </div>
